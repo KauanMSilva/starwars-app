5ac392c0fb766057ac39f4bce3eaf0479852184f
kauan <kauan_silva21@hotmail.com>
2024-11-30 20:07:39 -0300

    adicionax10

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,9 +10,9 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is {{ 10*count }}</button>
     <p>
-      Edit
+      Edito
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
