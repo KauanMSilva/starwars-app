556e117bb21cc7ad00802099107cb0f69cce5edc
César Augusto Costa <cezarcozta@gmail.com>
2024-12-01 04:31:51 -0300

    play

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,39 +4,34 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
-const mutiply = ref(10)
+const mutiply = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ mutiply*count }}</button>
-    <p>
-      Edito
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <input
+    :value="mutiply"
+    @input="event => mutiply = Number((event.target as HTMLInputElement).value)" type="number">
+  <button @click="count++">Multiply</button>
+  <div>
+    <h1>Result</h1>
+    <div class="row">
+      <p>{{ mutiply }}</p>
+      <p>X</p> 
+      <p>{{ count }}</p>
+      <p>=</p> 
+      <p>{{ mutiply * count }}</p>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.row{
+  display: flex;
+  gap: 10px;
 }
 </style>
