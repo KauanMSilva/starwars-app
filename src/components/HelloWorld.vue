79f34f59fd48ccaf4924bbb5d52191478818b2ce
kauan <kauan_silva21@hotmail.com>
2024-12-01 23:55:45 -0300

    edicao k.tech

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,75 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 const mutiply = ref(10)
+const botaoCorajoso = document.createElement('button');
+botaoCorajoso.textContent = 'SÓ CLIQUE CÊ TIVER CORAGEM MESMO';
+document.body.appendChild(botaoCorajoso);
+
+botaoCorajoso.style.position = 'fixed';
+botaoCorajoso.style.bottom = '20px';  
+botaoCorajoso.style.left = '50%';    
+botaoCorajoso.style.transform = 'translateX(-50%)'; 
+botaoCorajoso.style.padding = '10px 20px';
+botaoCorajoso.style.fontSize = '16px';
+botaoCorajoso.style.cursor = 'pointer';
+botaoCorajoso.style.backgroundColor = 'green';
+botaoCorajoso.style.border = 'bold';
+botaoCorajoso.style.color = 'pink';
+
+botaoCorajoso.addEventListener('click', function() {
+    alert('Parabéns, Você é corajoso mesmo ZéRuela!');
+});
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ mutiply*count }}</button>
+    <button type="button" @click="count++">K&C.Tech {{ mutiply*count }}</button>
     <p>
-      Edito
-      <code>components/HelloWorld.vue</code> to test HMR
+      
+      <code class="cor1">K & C. Tecnologia</code> 
     </p>
   </div>
 
   <p>
-    Check out
+    Todos os Direitos Reservados. <br><br>
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+      >Portifólio.</a
+    >
   </p>
-  <p>
-    Learn more about IDE Support for Vue in the
+  <p class="dev">
+    Desenvolvido por K&C.Tech Copyright © 2024
     <a
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
       target="_blank"
-      >Vue Docs Scaling up Guide</a
+      >Contato</a
     >.
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">Política de Privacidade</p>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+  font-weight : bold;
+  font-family : monospace;
+  
+}
+.cor1 {
+  color: red;
+  font-size: 50px;
+
+}
+
+.dev {
+  color: blueviolet;
+  font-weight : bold;
+  font-style: oblique;
+  justify-content: space-between;
 }
 </style>
